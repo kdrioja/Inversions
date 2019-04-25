5684c217eeb268022a445a9521855bb68ea11a19
Kenia Rioja <kdrioja@gmail.com>
2019-04-24 19:01:55 -0700

    Working on creating array

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,13 @@
+import java.util.ArrayList;
+
 public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public static ArrayList createArray(int n) {
+        ArrayList result = new ArrayList<Integer>(n);
+        
+        return result;
+    }
 }
